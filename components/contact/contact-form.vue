4bea8445a5374c41db1e173d213c2568b5aa43d0
James Williams <james.williams@techmeleon.co.uk>
2019-02-14 15:16:33 +0000

    Update contact-form.vue

--- components/contact/contact-form.vue
+++ components/contact/contact-form.vue
@@ -210,7 +210,7 @@ export default {
             this.sectionNumber += 1
             if (!this.inprogress) {
                 const _this = this
-                const url = '/api/contact'
+                const url = window.location.hostname + '/api/contact'
                 this.inprogress = true
                 this.$axios
                     .$post(url, { data: this.contact })
